1df38d38d04322ad27b5b54d1a0bcccc9c2525d0
[dyad] <dlh.upt.kota@gmail.com>
2026-04-15 09:30:43 +0700

    Reverted all changes back to version 09d1ebebdad35c32b2363a882f084f78a37d2826

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import CreateReport from "./pages/CreateReport";
+import EditReport from "./pages/EditReport";
+import ReportDetail from "./pages/ReportDetail";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,6 +19,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/create" element={<CreateReport />} />
+          <Route path="/edit/:id" element={<EditReport />} />
+          <Route path="/report/:id" element={<ReportDetail />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
