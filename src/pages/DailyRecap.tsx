1a3b939eb6369e386bbc4ea4df2c6edb700f8268
[dyad] <dlh.upt.kota@gmail.com>
2026-04-22 14:52:21 +0700

    [dyad] Mengurangi lebar kolom Keterangan menjadi 170px - wrote 2 file(s)

--- src/pages/DailyRecap.tsx
+++ src/pages/DailyRecap.tsx
@@ -490,7 +490,7 @@ const DailyRecap = () => {
               )}
               <col style={{ width: '100px' }} />
               <col style={{ width: '50px' }} />
-              <col style={{ width: '185px' }} />
+              <col style={{ width: '170px' }} />
             </colgroup>
             <thead className="pdf-table-header">
               <tr style={{ height: '40px' }}>
--- src/pages/MonthlyRecap.tsx
+++ src/pages/MonthlyRecap.tsx
@@ -502,7 +502,7 @@ const MonthlyRecap = () => {
               )}
               <col style={{ width: '100px' }} />
               <col style={{ width: '50px' }} />
-              <col style={{ width: '185px' }} />
+              <col style={{ width: '170px' }} />
             </colgroup>
             <thead className="pdf-table-header">
               <tr style={{ height: '40px' }}>
