291a403019184a353846b3536021bd3a1fdbd12b
[dyad] <dlh.upt.kota@gmail.com>
2026-05-11 14:06:50 +0700

    [dyad] Merapikan tata letak header dan tombol filter pada halaman Rekap Mingguan - wrote 3 file(s)

--- src/pages/FuelWeeklyRecap.tsx
+++ src/pages/FuelWeeklyRecap.tsx
@@ -347,7 +347,7 @@ const FuelWeeklyRecap = () => {
                     const subPertamaxLtr = items.reduce((acc, it) => acc + (it.fuel_type === 'Pertamax' ? (it.amount_liter || 0) : 0), 0);
                     const subDexliteRp = items.reduce((acc, it) => acc + (it.fuel_type === 'Dexlite' ? (it.amount_rp || it.amount) : 0), 0);
                     const subDexliteLtr = items.reduce((acc, it) => acc + (it.fuel_type === 'Dexlite' ? (it.amount_liter || 0) : 0), 0);
-                    const subOli = items.reduce((acc, it) => acc + (it.fuel_type === 'Oli' ? (it.amount_liter || item.amount) : 0), 0);
+                    const subOli = items.reduce((acc, it) => acc + (it.fuel_type === 'Oli' ? (it.amount_liter || it.amount) : 0), 0);
 
                     return (
                       <React.Fragment key={regionName}>
